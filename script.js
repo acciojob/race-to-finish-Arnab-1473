106742bed3e7371eb43143a54a47dd88beb850c9
Arnab-1473 <arban.nilr.1473@gmail.com>
2023-04-13 15:00:14 +0530

    updated

--- script.js
+++ script.js
@@ -2,3 +2,14 @@ window.promises = [];
 
 // Do not change the code above this
 // add your promises to the array `promises`
+const promises = [
+    new Promise((resolve) => setTimeout(() => resolve(Math.random() * 4+1),Math.random() * 4000+1000)),
+    new Promise((resolve) => setTimeout(() => resolve(Math.random() * 4+1),Math.random() * 4000+1000)),
+    new Promise((resolve) => setTimeout(() => resolve(Math.random() * 4+1),Math.random() * 4000+1000)),
+    new Promise((resolve) => setTimeout(() => resolve(Math.random() * 4+1),Math.random() * 4000+1000)),
+    new Promise((resolve) => setTimeout(() => resolve(Math.random() * 4+1),Math.random() * 4000+1000)),
+];
+
+Promise.any(promises).then(result => {
+    document.getElementById("output").innerHTML = `The first promise resolved with the result : ${result}`;
+});
